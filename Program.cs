2746e0e8c2c0441b2b3eed490f9a2de2784ee0d7
julian05z <89130623+julian05z@users.noreply.github.com>
2021-09-22 09:31:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace RandomNumber._2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomnumber;
+            Random randm = new Random();
+            randomnumber = randm.Next(1, 101);
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                string Titel = @"
+████████╗██╗  ██╗███████╗     ██████╗ ██████╗ ██╗ ██████╗ ██╗███╗   ██╗ █████╗ ██╗         ███╗   ██╗██╗   ██╗███╗   ███╗██████╗ ███████╗██████╗      ██████╗  █████╗ ███╗   ███╗███████╗
+╚══██╔══╝██║  ██║██╔════╝    ██╔═══██╗██╔══██╗██║██╔════╝ ██║████╗  ██║██╔══██╗██║         ████╗  ██║██║   ██║████╗ ████║██╔══██╗██╔════╝██╔══██╗    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝
+   ██║   ███████║█████╗      ██║   ██║██████╔╝██║██║  ███╗██║██╔██╗ ██║███████║██║         ██╔██╗ ██║██║   ██║██╔████╔██║██████╔╝█████╗  ██████╔╝    ██║  ███╗███████║██╔████╔██║█████╗  
+   ██║   ██╔══██║██╔══╝      ██║   ██║██╔══██╗██║██║   ██║██║██║╚██╗██║██╔══██║██║         ██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██╗██╔══╝  ██╔══██╗    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  
+   ██║   ██║  ██║███████╗    ╚██████╔╝██║  ██║██║╚██████╔╝██║██║ ╚████║██║  ██║███████╗    ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██████╔╝███████╗██║  ██║    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗
+   ╚═╝   ╚═╝  ╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+                                                                                                                                                                                         
+";
+                Console.WriteLine(Titel);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("Ich denke mir eine Zahl aus zwischen 1 und 100. Finde die Zahl heraus: ");
+            Console.WriteLine("----------------------------------------------------------------------");
+            int versuche = 1;
+            int notnumber = 2005;
+                while (notnumber == 2005)
+
+            try
+            {
+                int rnumber = Convert.ToInt32(Console.ReadLine());
+
+
+                while (rnumber != randomnumber)
+                {
+
+                    while (rnumber < 1 || rnumber > 100)
+                    {
+                        Console.WriteLine("Bitte gib eine gültige Zahl ein!!");
+                        rnumber = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("---------------------------------");
+                    }
+
+                    if (rnumber > randomnumber)
+                    {
+                        Console.WriteLine("Oh!, viel zu hoch, tippe eine neue Zahl ein");
+                        Console.WriteLine("-------------------------------------------");
+                    }
+
+                    else if (rnumber < randomnumber)
+                    {
+                        Console.WriteLine("Viel zu tief!, versuche es weiter");
+                        Console.WriteLine("---------------------------------");
+                    }
+
+                    versuche++;
+                    rnumber = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                if (rnumber == randomnumber)
+                {
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("Gratulation, du hast es geschaft");
+                    Console.WriteLine("Du hast " + versuche + " Versuche gebraucht.");
+                }
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Ungültige Eingabe, bitte halte dich an die Vorschriften!");
+                    continue;
+            }
+        }
+    }
+}
